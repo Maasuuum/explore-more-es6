c69eb85f97fd14924f6fce73c8bd4829894c7196
Maasuuum <fardousalmasum09@gmail.com>
2024-07-02 07:18:57 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,16 @@
+/**
+ * 1. var let const
+ * 2. default parameter
+ * 3. template string
+ * 4. arrow function
+ * 5. destructuring and spread operator
+ * 6. Object.keys, Object.values, Object.entries
+ */
+
+const a = 56;
+const numbers = [56,7,8]
+const person = {
+    name: 'sakib khan'
+}
+const message = `Hi, ${person.name} has a: ${a} access to ${numbers[2]}`
+console.log(message);
