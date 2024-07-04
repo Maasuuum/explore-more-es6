68277c2dec567b04cb54533632ec116f06e0c468
Maasuuum <fardousalmasum09@gmail.com>
2024-07-04 06:37:01 +0600

    filter and find in js

--- filter.js
+++ filter.js
@@ -2,5 +2,10 @@
 
 const numbers = [1, 5, 6, 4, 15];
 const players = [75, 65, 67, 72, 55, 59]
-const selected = players.filter(p => p > 70);
+// const selected = players.filter(p => p > 70);
+const selected = numbers.filter(p => p > 10);
 console.log(selected)
+
+const friends = ['Tina', 'Meena', 'Tisha','Raju', 'Mishu'];
+const oddFriends = friends.filter(friend => friend.length > 4)
+console.log(oddFriends);
---
+++ find.js
@@ -0,0 +1,5 @@
+const players = [75, 65, 67, 72, 55, 59];
+
+// const selected = players.find(p => p > 80);
+const selected = players.find(p => p > 50);
+console.log(selected);
