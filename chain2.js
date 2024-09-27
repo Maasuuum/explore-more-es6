789e4917c70377f5642d3538cd9d4993b4b8563f
Maasuuum <fardousalmasum09@gmail.com>
2024-09-27 11:26:18 +0600

    chaining revision

---
+++ chain2.js
@@ -0,0 +1,15 @@
+// data access
+const data = [{id: 1, name: 'abul', address: 'kochu khet'}];
+// console.log(data);
+// console.log(data[0]);
+// console.log(data[0].address);
+
+const products = {
+    count: 5000,
+    data: [
+        {id: 1, name: 'lenovo laptop', price: 65000},
+        {id: 2, name: 'macbook', price: 165000},
+    ]
+}
+// find out second product price
+console.log(products.data[1].price);
